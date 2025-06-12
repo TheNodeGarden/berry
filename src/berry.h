4a701949622ed7bdd12a9458f4ee632180feeada
David Thornley <thornley.david@nodegarden.com>
2025-06-12 13:29:15 +1000

    ng: Type fixes

--- src/berry.h
+++ src/berry.h
@@ -365,7 +365,7 @@ typedef bclass_ptr bclass_array[];
  *
  */
 #define be_native_module_attr_table(name)               \
-    static const bntvmodobj name##_attrs[] =
+    static const bntvmodobj_t name##_attrs[] =
 
 /**
  * @def be_native_module
@@ -404,13 +404,12 @@ typedef bclass_ptr bclass_array[];
  */
 #ifndef __cplusplus
 #define be_define_native_module(_name, _init)           \
-    const bntvmodule be_native_module(_name) = {        \
+    const bntvmodule_t be_native_module(_name) = {        \
         .name = #_name,                                 \
         .attrs = _name##_attrs,                         \
         .size = sizeof(_name##_attrs)                   \
                / sizeof(_name##_attrs[0]),              \
         .module = NULL,                                 \
-        .init = _init                                   \
     }
 #else
 #define be_define_native_module(_name, _init)           \
@@ -596,7 +595,7 @@ typedef bclass_ptr bclass_array[];
  *
  */
 #define be_local_closure(_name, _proto)       \
-  static const bclosure _name##_closure = {   \
+  static const struct bclosure _name##_closure = {   \
     NULL,             /**< bgcobject *next */ \
     BE_CLOSURE,       /**< type BE_CLOSURE */ \
     GC_CONST,         /**< marked GC_CONST */ \
